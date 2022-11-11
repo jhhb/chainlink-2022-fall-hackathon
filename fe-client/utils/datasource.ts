104fccd30a30935d3adc7c625f3f9e2f3fc2fa59
James Boyle <jhhboyle@gmail.com>
2022-11-11 12:09:19 -0600

    Add assertions, remove ts-ignore from data source

--- fe-client/utils/datasource.ts
+++ fe-client/utils/datasource.ts
@@ -1,5 +1,7 @@
 import {
+  ALL_STATUSES,
   CONTRACT_ABI,
+  GET_ANSWER_FUNCTION_NAME,
   GET_STATUS_FUNCTION_NAME,
   LOCALHOST_CONTRACT_ADDRESS,
   START_FUNCTION_NAME,
@@ -9,13 +11,13 @@ import Moralis from "moralis-v1";
 import { Statuses } from "../components/AuthenticatedActions";
 
 export async function fetchStatus(address: string): Promise<Statuses> {
-  // @ts-ignore
-  const result: Statuses = await Moralis.executeFunction({
+  const result = await Moralis.executeFunction({
     abi: CONTRACT_ABI,
     contractAddress: LOCALHOST_CONTRACT_ADDRESS,
     functionName: GET_STATUS_FUNCTION_NAME,
     params: { addr: address },
   });
+  assertIsStatuses(result);
   return result;
 }
 
@@ -28,4 +30,34 @@ export async function askQuestion(): Promise<
     functionName: START_FUNCTION_NAME,
     params: {},
   });
+}
+
+export async function getAnswer(address: string): Promise<string> {
+  const result = await Moralis.executeFunction({
+    abi: CONTRACT_ABI,
+    contractAddress: LOCALHOST_CONTRACT_ADDRESS,
+    functionName: GET_ANSWER_FUNCTION_NAME,
+    params: { userAddress: address },
+  });
+  assertIsString(result);
+  return result;
+}
+
+function assertIsString(value: unknown): asserts value is string {
+  if (typeof value !== "string") {
+    throw new Error(
+      `Expected value [${value}] to be a string; got: [${typeof value}]`
+    );
+  }
+}
+
+function assertIsStatuses(value: unknown): asserts value is Statuses {
+  assertIsString(value);
+
+  const castValue = value as Statuses;
+  if (!ALL_STATUSES.includes(castValue)) {
+    throw new Error(
+      `Expected value [${value}] to be a Statuses; got: [${value}]`
+    );
+  }
 }
