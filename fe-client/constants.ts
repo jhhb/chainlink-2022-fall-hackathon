13bbc1827bd758456aab9174825b32457a80f0d3
James Boyle <jhhboyle@gmail.com>
2022-11-12 14:41:49 -0600

    Remove not needed old deploy + rename deploy

--- fe-client/constants.ts
+++ fe-client/constants.ts
@@ -1,160 +1,167 @@
 const CONTRACT_ABI = [
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint64",
-        "name": "_subscriptionId",
-        "type": "uint64"
+        internalType: "uint64",
+        name: "_subscriptionId",
+        type: "uint64",
       },
       {
-        "internalType": "address",
-        "name": "vrfCoordinator",
-        "type": "address"
+        internalType: "address",
+        name: "vrfCoordinator",
+        type: "address",
       },
       {
-        "internalType": "bytes32",
-        "name": "_keyHash",
-        "type": "bytes32"
-      }
+        internalType: "bytes32",
+        name: "_keyHash",
+        type: "bytes32",
+      },
     ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "have",
-        "type": "address"
+        internalType: "address",
+        name: "have",
+        type: "address",
       },
       {
-        "internalType": "address",
-        "name": "want",
-        "type": "address"
-      }
+        internalType: "address",
+        name: "want",
+        type: "address",
+      },
     ],
-    "name": "OnlyCoordinatorCanFulfill",
-    "type": "error"
+    name: "OnlyCoordinatorCanFulfill",
+    type: "error",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "requestId",
-        "type": "uint256"
+        indexed: true,
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
       },
       {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "asker",
-        "type": "uint256"
-      }
+        indexed: true,
+        internalType: "uint256",
+        name: "asker",
+        type: "uint256",
+      },
     ],
-    "name": "QuestionAnswered",
-    "type": "event"
+    name: "QuestionAnswered",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "requestId",
-        "type": "uint256"
+        indexed: true,
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "asker",
-        "type": "address"
-      }
+        indexed: true,
+        internalType: "address",
+        name: "asker",
+        type: "address",
+      },
     ],
-    "name": "QuestionAsked",
-    "type": "event"
+    name: "QuestionAsked",
+    type: "event",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "userAddress",
-        "type": "address"
-      }
+        internalType: "address",
+        name: "userAddress",
+        type: "address",
+      },
     ],
-    "name": "answer",
-    "outputs": [
+    name: "answer",
+    outputs: [
       {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "askQuestion",
-    "outputs": [
+    inputs: [],
+    name: "askQuestion",
+    outputs: [
       {
-        "internalType": "uint256",
-        "name": "requestId",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
+      },
     ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "addr",
-        "type": "address"
-      }
+        internalType: "address",
+        name: "addr",
+        type: "address",
+      },
     ],
-    "name": "getUserStatus",
-    "outputs": [
+    name: "getUserStatus",
+    outputs: [
       {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "requestId",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
       },
       {
-        "internalType": "uint256[]",
-        "name": "randomWords",
-        "type": "uint256[]"
-      }
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
     ],
-    "name": "rawFulfillRandomWords",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+    name: "rawFulfillRandomWords",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
 // TODO: JB - FIX THIS.
-const LOCALHOST_CONTRACT_ADDRESS = "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c";
-const START_FUNCTION_NAME = 'askQuestion';
-const GET_STATUS_FUNCTION_NAME = 'getUserStatus';
-const GET_ANSWER_FUNCTION_NAME = 'answer';
+const LOCALHOST_CONTRACT_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+const START_FUNCTION_NAME = "askQuestion";
+const GET_STATUS_FUNCTION_NAME = "getUserStatus";
+const GET_ANSWER_FUNCTION_NAME = "answer";
 
-const STATUS_NONE = 'NONE';
-const STATUS_RUNNING = 'RUNNING';
-const STATUS_RAN = 'RAN';
-type StatusTuple = ['RAN', 'NONE', 'RUNNING'];
+const STATUS_NONE = "NONE";
+const STATUS_RUNNING = "RUNNING";
+const STATUS_RAN = "RAN";
+type StatusTuple = ["RAN", "NONE", "RUNNING"];
 const ALL_STATUSES: StatusTuple = [STATUS_RAN, STATUS_NONE, STATUS_RUNNING];
 
-export { CONTRACT_ABI, LOCALHOST_CONTRACT_ADDRESS, START_FUNCTION_NAME, GET_STATUS_FUNCTION_NAME, GET_ANSWER_FUNCTION_NAME, ALL_STATUSES};
+export {
+  CONTRACT_ABI,
+  LOCALHOST_CONTRACT_ADDRESS,
+  START_FUNCTION_NAME,
+  GET_STATUS_FUNCTION_NAME,
+  GET_ANSWER_FUNCTION_NAME,
+  ALL_STATUSES,
+};
